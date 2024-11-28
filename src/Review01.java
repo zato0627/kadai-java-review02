9557be1e42c9090198e32a9bccd7e072f8646762
zato0627 <panmimi38@icloud.com>
2024-11-28 17:53:24 +0900

    実行結果に表示された　.0　を表示させないように改善した。

--- src/Review01.java
+++ src/Review01.java
@@ -3,16 +3,21 @@ public class Review01 {
 
 	public static void main(String[] args) {
 
-		double cash = 1500;
-		double tax = getTax(1500, 0.1);
+		//cash=元の金額,tax=税込み金額,base=消費税
+		double cash = 1985;
+		double tax = getTax(1985, 0.1);
 		double base = tax - cash;
-		System.out.println(cash + "円の商品の税込み価格は" + tax + "円（消費税は" + base + "円）です");
+
+		//doubleからintに変換して表示
+		System.out.println((int)cash + "円の商品の税込み価格は" + (int)tax + "円（消費税は" + (int)base + "円）です");
 
 	}
 	public static  double getTax(double cash, double tax) {
 		//消費税額を計算してmainに戻す
 
+		//消費税を計算
 		double base = cash * tax;
+		//税込み金額を計算して戻す
 		double result = cash + base;
 		return result;
 	}
