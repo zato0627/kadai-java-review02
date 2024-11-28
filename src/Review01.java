41899f694b81c5ee0c8b509e9be42f0c393a30a1
zato0627 <panmimi38@icloud.com>
2024-11-28 18:43:23 +0900

    メソッド名を変更、計算式を変更

--- src/Review01.java
+++ src/Review01.java
@@ -3,23 +3,22 @@ public class Review01 {
 
 	public static void main(String[] args) {
 
-		//cash=元の金額,tax=税込み金額,base=消費税
-		double cash = 1985;
-		double tax = getTax(1985, 0.1);
-		double base = tax - cash;
+		//cash=元の金額,tax=消費税,taxin=税込み価格
+		int cash = 1985;
+		double tax = tax(1985, 0.1);
+		int taxin = cash + (int)tax;
 
-		//doubleからintに変換して表示
-		System.out.println((int)cash + "円の商品の税込み価格は" + (int)tax + "円（消費税は" + (int)base + "円）です");
+
+		System.out.println(cash + "円の商品の税込み価格は" + taxin + "円（消費税は" + (int)tax + "円）です");
 
 	}
-	public static  double getTax(double cash, double tax) {
-		//消費税額を計算してmainに戻す
+	public static  double tax(double cash, double tax) {
+		//消費税を計算してmainに戻す
 
 		//消費税を計算
-		double base = cash * tax;
-		//税込み金額を計算して戻す
-		double result = cash + base;
-		return result;
+		double taxin = cash * tax;
+
+		return taxin;
 	}
 
 }
